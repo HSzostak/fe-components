e3a8576cb024fdf219e2abbb7ca33da8d1426d27
Henrique Szostak <henrique.szostak@mirumagency.com>
2017-11-21 10:08:37 -0200

    Adding Nunjucks to the build

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,33 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fs = require('fs');
+
+function generateHtmlPlugins (templateDir) {
+  // Read files in tempalte directory
+  const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
+
+  return templateFiles.map(item => {
+    // Split names and extension
+    const parts = item.split('.'),
+          name = parts[0],
+          extension = parts[1];
+
+    // Create new HtmlWebpackPlugin with options
+    return new HtmlWebpackPlugin({
+      filename: `${name}.html`,
+      template: 'nunjucks-html-loader!' + path.resolve(__dirname, `${templateDir}/${name}.${extension}`)
+    });
+  });
+}
+
+const htmlPlugins = generateHtmlPlugins('./source/templates/views');
 
 module.exports = {
   entry: ['./source/scripts/app.js', './source/scss/main.scss'],
-  output: {
-    filename: 'scripts.bundle.js',
-    path: path.resolve(__dirname, 'dist/js')
+  devServer: {
+    contentBase: './dist'
   },
   module: {
     rules: [
@@ -19,15 +41,32 @@ module.exports = {
       { // sass / scss loader for webpack
         test: /\.(sass|scss)$/,
         loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      },
+      {
+        test: /\.html$|njk|nunjucks/,
+        use: ['html-loader',{
+          loader: 'nunjucks-html-loader',
+          options : {
+            searchPaths: ['./source/templates/', './source/templates/views', './source/templates/components']
+          }
+        }]
       }
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new ExtractTextPlugin({ // define where to save the file
-      filename: '../css/styles.bundle.css',
-      allChunks: true,
-    }),
-  ],
+      filename:  (getPath) => {
+        return getPath('css/styles.bundle.css').replace('css', 'css');
+      },
+      allChunks: true
+    })
+  ]
+  .concat(htmlPlugins),
+  output: {
+    filename: 'scripts.bundle.js',
+    path: path.resolve(__dirname, 'dist/')
+  },
   stats: {
     colors: true
   }
